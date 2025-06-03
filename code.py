cf6b73da0f54953de179d106d3c9acf28bcc1f9e
Paul M <dufus2506@gmail.com>
2025-06-03 13:02:23 -0400

    Update code.py
    Added version comment line

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # Next MBTA Ride Sign
 # 2025 Paul M
 # SPDX-License-Identifier: MIT
-
+# Version 1.0
 # Creates a sign that shows when the next bus or train will arrive at an MBTA stop
 # Stops can be commuter rail stops, subway stations, or bus stops
 
